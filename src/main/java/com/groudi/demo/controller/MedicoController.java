f31e2b24b459a76f1ee028502adcc5a8088e752b
Davi-pontes <davimartins2450@gmail.com>
2024-12-03 14:09:01 -0300

    Adde new logic

--- src/main/java/com/groudi/demo/controller/MedicoController.java
+++ src/main/java/com/groudi/demo/controller/MedicoController.java
@@ -1,6 +1,11 @@
 package com.groudi.demo.controller;
 
 import com.groudi.demo.medico.DadosCadastroMedicos;
+import com.groudi.demo.medico.Medico;
+import com.groudi.demo.medico.MedicoRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedicos dados){
-        System.out.println(dados);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedicos dados){
+        repository.save(new Medico(dados));
+
     }
 }
---
+++ src/main/java/com/groudi/demo/medico/MedicoRepository.java
@@ -0,0 +1,6 @@
+package com.groudi.demo.medico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico,Long> {
+}
