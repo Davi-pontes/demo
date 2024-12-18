42bd947e995dd99468edb81eec2a6810b7292d36
Davi-pontes <davimartins2450@gmail.com>
2024-12-18 10:51:12 -0300

    feat: Added route to update and delete paciente.

--- src/main/java/com/groudi/demo/controller/PacienteController.java
+++ src/main/java/com/groudi/demo/controller/PacienteController.java
@@ -1,18 +1,17 @@
 package com.groudi.demo.controller;
 
-import com.groudi.demo.paciente.DadosCadastroPacientes;
-import com.groudi.demo.paciente.Paciente;
-import com.groudi.demo.paciente.PacienteRepository;
+import com.groudi.demo.paciente.*;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/paciente")
+@RequestMapping("/pacientes")
 public class PacienteController {
     @Autowired
     private PacienteRepository repository;
@@ -21,5 +20,28 @@ public class PacienteController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroPacientes dados){
         repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = "nome")Pageable paginacao){
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public  void  atualizar (@RequestBody @Valid DadosAtualizacaoPacientes dados){
+        var paciente = repository.getReferenceById(dados.id());
+
+        paciente.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity excluir(@PathVariable Long id){
+        var paciente = repository.getReferenceById(id);
+
+        paciente.excluir();
+
+        return ResponseEntity.noContent().build();
     }
 }
